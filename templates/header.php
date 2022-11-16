1dccccddb43c6501f4742c2f8bbc815570e2bbe0
truongbq31 <ytbsadboy@gmail.com>
2022-11-16 16:03:17 +0700

    header

--- templates/header.php
+++ templates/header.php
@@ -104,6 +104,8 @@
 								<!-- end dropdown -->
 
 								<a href="login.php" class="header__sign-in" <?php if(isset($_SESSION['admin']) || isset($_SESSION['user'])){
+                                    echo "hidden";
+                                }else{
                                     echo "";
                                 } ?>>
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -111,6 +113,23 @@
 									</svg>
 									<span>Đăng nhập</span>
 								</a>
+
+                                <a href="logout.php" class="header__sign-in" <?php if(isset($_SESSION['admin']) || isset($_SESSION['user'])){
+                                    echo "";
+                                }else{
+                                    echo "hidden";
+                                } ?>>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <path d="M20,12a1,1,0,0,0-1-1H11.41l2.3-2.29a1,1,0,1,0-1.42-1.42l-4,4a1,1,0,0,0-.21.33,1,1,0,0,0,0,.76,1,1,0,0,0,.21.33l4,4a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42L11.41,13H19A1,1,0,0,0,20,12ZM17,2H7A3,3,0,0,0,4,5V19a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V16a1,1,0,0,0-2,0v3a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V8a1,1,0,0,0,2,0V5A3,3,0,0,0,17,2Z" />
+                                    </svg>
+                                    <span>Đăng Xuất</span>
+                                </a>
+
+                                <a <?php if(isset($_SESSION['admin'])){
+                                    echo "";
+                                }else{
+                                    echo "hidden";
+                                } ?> href="admin/home.php">Trang quản trị</a>
 
 							</div>
 							<!-- end header auth -->
