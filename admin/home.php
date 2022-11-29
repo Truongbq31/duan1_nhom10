508261aed700e4a76e82c925511acf706cfe7812
truongbq31 <ytbsadboy@gmail.com>
2022-11-29 21:14:00 +0700

    29/11

--- admin/home.php
+++ admin/home.php
@@ -1,9 +1,4 @@
 <?php
-//session_start();
-//if(isset($_SESSION['admin'])){
 include ("controllers/c_home.php");
 $thong_ke = new c_home();
-$thong_ke->thong_ke_website();
-//}else{
-//    echo "<script>alert('Cần đăng nhập để vào admin')</script>";
-//}
+$thong_ke->thong_ke();
--- admin/models/m_home.php
+++ admin/models/m_home.php
@@ -1,33 +1,10 @@
 <?php
 require_once('database.php');
 class m_home extends database{
-    // xây dựng lấy danh sách hàng hóa
-//    public function thong_ke_hang_hoa(){
-//        $sql = "SELECT COUNT(id) as sl_hh from phim ";//chuỗi câu truy vấn dữ liệu
-//        $this->setQuery($sql);
-//        //lấy nhiều dòng dữ liệu(Trả về 1 mảng dữ liệu)
-//        return $this->loadAllRows();
-//    }
-
-//    public function thong_ke_khach_hang(){
-//        $sql = "SELECT COUNT(id) as sl_kh from nguoi_dung";//chuỗi câu truy vấn dữ liệu
-//        $this->setQuery($sql);
-//        //lấy nhiều dòng dữ liệu(Trả về 1 mảng dữ liệu)
-//        return $this->loadAllRows();
-//    }
-//    public function thong_ke_loai_hang(){
-//        $sql = "SELECT COUNT(id) as sl_lh from loai_phim";//chuỗi câu truy vấn dữ liệu
-//        $this->setQuery($sql);
-//        //lấy nhiều dòng dữ liệu(Trả về 1 mảng dữ liệu)
-//        return $this->loadAllRows();
-//    }
-
-//    public function thong_ke_binh_luan(){
-//        $sql = "SELECT COUNT(id) as sl_bl from binh_luan";//chuỗi câu truy vấn dữ liệu
-//        $this->setQuery($sql);
-//        //lấy nhiều dòng dữ liệu(Trả về 1 mảng dữ liệu)
-//        return $this->loadAllRows();
-//    }
-
-
+    public function thong_ke(){
+        $sql = "SELECT * FROM ve";//chuỗi câu truy vấn dữ liệu
+        $this -> setQuery($sql);
+        //lấy nhiều dòng dữ liệu(Trả về 1 mảng dữ liệu)
+        return $this -> loadAllRows();
+    }
 }
