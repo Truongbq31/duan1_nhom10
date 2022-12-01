89181a236b062f9930f5fb51de491d891090e489
manhtuong <lamt5805@gmail.com>
2022-12-01 17:16:42 +0700

    update 19/11

--- models/m_data.php
+++ models/m_data.php
@@ -9,7 +9,14 @@
       inner join khung_gio_chieu on khung_gio_chieu.id=lich_chieu.id_khung_gio_chieu
       inner join khung_gio_phong_chieu on khung_gio_chieu.id=khung_gio_phong_chieu.id_khung_gio_chieu
       inner join phong_chieu on khung_gio_phong_chieu.id_phong_chieu=phong_chieu.id
-      where lich_chieu.id='$key'";
+      where chi_nhanh.id='$key'";
+//$sql = "Select *, phim.name as ten_phim, chi_nhanh.ten_chi_nhanh,khung_gio_chieu.gio_bat_dau,phong_chieu.ten_phong,lich_chieu.id as id_lich_chieu from lich_chieu  inner join chi_nhanh_phim on chi_nhanh_phim.id=lich_chieu.id_chi_nhanh_phim
+//      inner join phim on chi_nhanh_phim.id_phim = phim.id
+//      inner join chi_nhanh on chi_nhanh_phim.id_chi_nhanh=chi_nhanh.id
+//      inner join khung_gio_chieu on khung_gio_chieu.id=lich_chieu.id_khung_gio_chieu
+//      inner join khung_gio_phong_chieu on khung_gio_chieu.id=khung_gio_phong_chieu.id_khung_gio_chieu
+//      inner join phong_chieu on khung_gio_phong_chieu.id_phong_chieu=phong_chieu.id
+//      where chi_nhanh.id='$key'";
       $result = $conn ->query($sql)->fetchAll();
    foreach ($result as $index => $value){
 ?>
