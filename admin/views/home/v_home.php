32e7527774738c48b808abc7f6b0c034134f4f9f
anhloi0987181979 <108559319+anhloi0987181979@users.noreply.github.com>
2022-12-09 22:42:02 +0700

    Add files via upload

---
+++ admin/views/home/v_home.php
@@ -0,0 +1,281 @@
+<header class="header">
+	<div class="header__content">
+		<!-- header logo -->
+		<a href="index.html" class="header__logo">
+			<img src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/img/logo.svg" alt="">
+		</a>
+		<!-- end header logo -->
+
+		<!-- header menu btn -->
+		<button class="header__btn" type="button">
+			<span></span>
+			<span></span>
+			<span></span>
+		</button>
+		<!-- end header menu btn -->
+	</div>
+</header>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/css/magnific-popup.css">
+	<link rel="stylesheet" href="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/css/jquery.mCustomScrollbar.min.css">
+	<link rel="stylesheet" href="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/css/select2.min.css">
+	<link rel="stylesheet" href="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/css/admin.css">
+
+	<!-- Favicons -->
+	<link rel="icon" type="image/png" href="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/icon/favicon-32x32.png" sizes="32x32">
+	<link rel="apple-touch-icon" href="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/icon/favicon-32x32.png">
+
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="Dmitry Volkov">
+	<script src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/js/jquery-3.6.0.min.js"></script>
+	<script src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/js/bootstrap.bundle.min.js"></script>
+	<script src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/js/jquery.magnific-popup.min.js"></script>
+	<script src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/js/jquery.mousewheel.min.js"></script>
+	<script src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/js/jquery.mCustomScrollbar.min.js"></script>
+	<script src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/js/select2.min.js"></script>
+	<script src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/js/admin.js"></script>
+	<title>FlixGo – Online Movies, TV Shows & Cinema HTML Template</title>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+	<script src="public/assetss/libs/jquery/distt/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+   
+
+</head>
+<main class="main">
+	<div class="container-fluid">
+		<div class="row">
+			<!-- main title -->
+			<div class="col-12">
+				<div class="main__title">
+					<h2>Dashboard</h2>
+
+					<a href="add-item.html" class="main__title-link">add item</a>
+				</div>
+			</div>
+			<!-- end main title -->
+		</div>
+
+		<div class="row row--grid">
+			<!-- stats -->
+			<div class="col-12 col-sm-6 col-xl-3">
+				<div class="stats">
+					<span>Tổng Cặp Ghế Đặt</span>
+					<p><?= $sum_all->Sum_ghe_ngoi ?></p>
+					<img src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/img/graph-bar.svg" alt="">
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-xl-3">
+				<div class="stats">
+					<span>Tổng Comment</span>
+					<p><?= $sum_comment->tongcomment ?></p>
+					<img src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/img/graph-bar.svg" alt="">
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-xl-3">
+				<div class="stats">
+					<span>Tổng Phim Được Chiếu</span>
+					<p><?= $sum_all->Sum_phim ?></p>
+					<img src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/img/graph-bar.svg" alt="">
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-xl-3">
+				<div class="stats">
+					<span>Tổng Tiền</span>
+					<p><?= $sum_all->tong_tien ?></p>
+					<img src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/img/graph-bar.svg" alt="">
+				</div>
+			</div>
+
+
+			<!-- dashbox -->
+			<div class="col-12 col-xl-6">
+				<div class="dashbox">
+					<div class="dashbox__title">
+						<h3><img src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/img/award.svg" alt=""> Phim Nổi Bật</h3>
+
+						<div class="dashbox__wrap">
+							<a class="dashbox__refresh" href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<path d="M21,11a1,1,0,0,0-1,1,8.05,8.05,0,1,1-2.22-5.5h-2.4a1,1,0,0,0,0,2h4.53a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4.77A10,10,0,1,0,22,12,1,1,0,0,0,21,11Z" />
+								</svg></a>
+							<a class="dashbox__more" href="catalog.html">View All</a>
+						</div>
+					</div>
+
+					<div class="dashbox__table-wrap">
+						<table class="main__table main__table--dash">
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Tên Phim</th>
+									
+									<th>Giá Trung Bình</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach ($result as $item => $value) { ?>
+									<tr>
+										<td>
+											<div class="main__table-text"><?= $value->id ?></div>
+										</td>
+										<td>
+											<div class="main__table-text"><?= $value->name ?></div>
+										</td>
+										
+										<td>
+											<div class="main__table-text "><?= $value->gia_trung_binh ?></div>
+										</td>
+									</tr>
+								<?php } ?>
+
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+			<!-- end dashbox -->
+
+			<!-- dashbox -->
+			<div class="col-12 col-xl-6">
+				<div class="dashbox">
+					<div class="dashbox__title">
+						<h3><img src="abcd/dmitryvolkov.me/demo/flixgo2.0/admin/img/film.svg" alt="">Chi Tiết Thống Kê</h3>
+
+						<div class="dashbox__wrap">
+							<a class="dashbox__refresh" href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<path d="M21,11a1,1,0,0,0-1,1,8.05,8.05,0,1,1-2.22-5.5h-2.4a1,1,0,0,0,0,2h4.53a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4.77A10,10,0,1,0,22,12,1,1,0,0,0,21,11Z" />
+								</svg></a>
+							<a class="dashbox__more" href="catalog.html">View All</a>
+						</div>
+					</div>
+
+					<div class="dashbox__table-wrap">
+						<table class="main__table main__table--dash">
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Tên Phim</th>
+									<th>Giá Thấp Nhất</th>
+									<th>Giá Cao Nhất</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach ($result as $item => $value) { ?>
+									<tr>
+										<td>
+											<div class="main__table-text"><?= $value->id ?></div>
+										</td>
+										<td>
+											<div class="main__table-text"><?= $value->name ?></div>
+										</td>
+										<td>
+											<div class="main__table-text"><?= $value->gia_thap_nhat ?></div>
+										</td>
+										<td>
+											<div class="main__table-text "><?= $value->gia_cao_nhat ?></div>
+										</td>
+									</tr>
+								<?php } ?>
+
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+			<!-- end dashbox -->
+
+			<!-- dashbox -->
+
+			<!-- end dashbox -->
+		</div>
+	</div>
+
+	<div class="page-wrapper" >
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-9">
+					<div class="card">
+						
+						<div class="card-body">
+							<div class="row">
+								<canvas id="myChart" style="width:100%;max-width:600px;margin: auto;"></canvas>
+								<script>
+									<?php 
+									$array_phim=[];
+									foreach($result as $item => $value){
+										$array_phim[]=$value->name;
+									} ?>
+									
+									 xValues = <?php echo json_encode($array_phim) ?>;
+									
+									 <?php 
+									$array_gia=[];
+									foreach($result as $item => $value){
+										$array_gia[]=$value->gia_cao_nhat;
+									} ?>
+									var yValues =  <?php echo json_encode($array_gia) ?>;
+									console.log(xValues);
+									var barColors = [
+										"#b91d47",
+										"#00aba9",
+										"#2b5797",
+
+
+									];
+									new Chart("myChart", {
+										type: "pie",
+										data: {
+											labels: xValues,
+											datasets: [{
+												backgroundColor: barColors,
+												data: yValues
+											}]
+										},
+										options: {
+											title: {
+												display: true,
+												text: ""
+											}
+										}
+									});
+								</script>
+
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+<script src="public/assetss/libs/popper.js/distt/umd/popper.min.js"></script>
+    <script src="public/assetss/libs/bootstrap/distt/js/bootstrap.min.js"></script>
+    <script src="public/assetss/libs/perfect-scrollbar/distt/perfect-scrollbar.jquery.min.js"></script>
+    <script src="public/assetss/extra-libs/sparkline/sparkline.js"></script>
+    <!--Wave Effects -->
+    <script src="public/distt/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="public/distt/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="public/distt/js/custom.min.js"></script>
+    
+    <!--This page JavaScript -->
+    <!-- <script src="public/distt/js/pages/dashboards/dashboard1.js"></script> -->
+    <!-- Charts js Files -->
+    <script src="public/assetss/libs/flot/excanvas.js"></script>
+    <script src="public/assetss/libs/flot/jquery.flot.js"></script>
+    <script src="public/assetss/libs/flot/jquery.flot.pie.js"></script>
+    <script src="public/assetss/libs/flot/jquery.flot.time.js"></script>
+    <script src="public/assetss/libs/flot/jquery.flot.stack.js"></script>
+    <script src="public/assetss/libs/flot/jquery.flot.crosshair.js"></script>
+    <script src="public/assetss/libs/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
+    <script src="public/distt/js/pages/chart/chart-page-init.js"></script>
