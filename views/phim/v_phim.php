5c99f2c5bc1fa4a532e2e71bf54407cfcbf06f97
truongbq31 <ytbsadboy@gmail.com>
2022-12-10 17:21:17 +0700

    10/12

--- views/phim/v_phim.php
+++ views/phim/v_phim.php
@@ -28,7 +28,7 @@
 			<div class="col-6 col-sm-4 col-lg-3 col-xl-2">
 				<div class="card">
 					<a href="details.php?id=<?php echo $value->id_phim?>" class="card__cover">
-						<img src="admin /public/img/<?php echo $value->img?>" alt="">
+						<img height="250px" src="admin /public/img/<?php echo $value->img?>" alt="">
 						<span class="card__play">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z"/></svg>
 						</span>
